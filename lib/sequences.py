44227ff1dbf227bc43aaf7bed7f753afff532ed9
Noah <noahhughesph@gmail.com>
2023-09-28 21:34:37 -0400

    all tests passing, finally

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
+
 def print_fibonacci(length):
-    pass
+    if (length == 0):
+        print([])
+    elif (length == 1):
+        print([0])
+    else:
+        sequence = [0, 1]
+        for _ in range(2, length):
+            next_num = sequence[-1] + sequence[-2]
+            sequence.append(next_num)
+        print(sequence)
